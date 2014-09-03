956de79e72e11d574cad413ac0325decea95a44e
RichieHowell <richieh@vt.edu>
2014-09-03 14:06:24 -0400

    In class 1
    the first inclass exercise

---
+++ RichardHowell_Inclass1.R
@@ -0,0 +1,9 @@
+setwd("C:\\Users\\Richie Howell\\Documents\\R Stuffs and things\\CDMA-R")#number 2
+cars1=read.csv("cars1.csv")#number3
+dimensions=dim(cars1)#number 3 again
+var1=cars1[2,2]#number 4
+#number 5 : the character names are Speed and Distance
+cars1[,1]#number 6 part 1
+cars1[,2]#number 6 part 2
+SPEED = cars1$speed#number 7
+cars1[15,]#number 8
