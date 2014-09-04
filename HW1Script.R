cbed4b55a9a8d1e036e17f49cb987d723ebd8487
RichieHowell <richieh@vt.edu>
2014-09-04 09:38:55 -0400

    HW1 first commit
    first commit of the homework i worked with in a local file system

--- HW1Script.R
+++ HW1Script.R
@@ -5,11 +5,22 @@ setwd("C:\\Users\\Richie Howell\\Documents\\R Stuffs and things")
 table = read.table(file = "HW1text.txt", header = TRUE)
 #-----------------------------------------------------------
 #Problem 2
+#create the vectors for number 1
 vec1=c(1,0,0,0)
 vec2=c(0,1,0,0)
 vec3=c(0,0,1,0)
 vec4=c(0,0,0,1)
+#binds the vectors into a matrix, identity matrix in this example
 MAT1=cbind(vec1,vec2,vec3,vec4)
-lastElement = MAT1[4,4]
+#outputs the last element in the matrix
+print(MAT1[4,4])
+#finds the transpose of the matrix and puts it into MAT1.transposed
 MAT1.Transposed = t(MAT1)
+#finds the inverse of the matrix and puts it into MAT1.Inverse
 Mat1.Inverse=solve(MAT1)
+#------------------------------------------------
+#problem 3
+#creates the data set
+fpe <- read.table("http://data.princeton.edu/wws509/datasets/effort.dat")
+#prints out the data where effort = 0
+print(fpe[fpe$effort == 0,])
